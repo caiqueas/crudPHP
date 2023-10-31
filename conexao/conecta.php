0aef504636c772709edcfd3bbc6e4093373b4fa3
caiqueas <107781070+caiqueas@users.noreply.github.com>
2023-10-31 08:18:45 -0300

    Update conecta.php

--- conexao/conecta.php
+++ conexao/conecta.php
@@ -1,16 +1,32 @@
-<?php
-
-$mysqlhostname = "179.188.16.17";
-$mysqlport = "3306";
-$mysqlusername = "webcamadas";
-$mysqlpassword = "Locaweb@102030";
-$mysqldatabase = "webcamadas";
-
-//Mostra a String de Conexao ao MySQL → Criei a String de conexão e conectei ao banco (PDO)
-$dsn = 'mysql:host=' . $mysqlhostname . ';dbname=' . $mysqldatabase . ';port=' . $mysqlport;
-$pdo = new PDO($dsn, $mysqlusername, $mysqlpassword);
-
-$pdo->lastInsertId()
-
-?>
-
+<?php
+
+
+
+$mysqlhostname = "HostDoSeuBanco";
+
+$mysqlport = "3306";
+
+$mysqlusername = "Username";
+
+$mysqlpassword = "SuaSenha";
+
+$mysqldatabase = "NomeDoBanco";
+
+
+
+//Mostra a String de Conexao ao MySQL → Criei a String de conexão e conectei ao banco (PDO)
+
+$dsn = 'mysql:host=' . $mysqlhostname . ';dbname=' . $mysqldatabase . ';port=' . $mysqlport;
+
+$pdo = new PDO($dsn, $mysqlusername, $mysqlpassword);
+
+
+
+$pdo->lastInsertId()
+
+
+
+?>
+
+
+
